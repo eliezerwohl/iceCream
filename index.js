199f3df08f82b60b354d1fb0abd05411182bccf0
eliezerwohl <eliezerwohl@gmail.com>
2016-02-10 18:05:09 -0500

    handelbar now work, display data locally

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var expressHandlebars = require("express-handlebars");
 var PORT = 8080 || process.env.NODE_ENV;
 var mysql = require('mysql');
 
-app.engine('handlebars', expressHandlebars({defaultLayout:'harry'}));
+app.engine('handlebars', expressHandlebars({defaultLayout:'main'}));
 app.set('view engine', 'handlebars');
 
 var connection = mysql.createConnection({
@@ -20,19 +20,15 @@ connection.connect(function(err){
   }
 });
 
-var icecreams = [
-{name: 'vanilla', price: 10, awesomeness: 3},
-{name: 'chocolate', price: 4, awesomeness: 8},
- {name: 'banana', price: 1, awesomeness: 1}, 
- {name: 'greentea', price: 5, awesomeness: 7}, 
- {name: 'jawbreakers', price: 6, awesomeness: 2}, 
- ];
-
 app.get("/", function (req, res){
-  connection.query("select * from iceList where ID =1 ", function(err, results){
-    console.log(results);
+  connection.query("select * from iceList", function(err, results){
+    console.log(results)
+    
+    var data = {
+      iceCream: results
+    }
+    res.render('iceCream', data);
   })
-res.send(icecreams.name);
 });
  app.listen(PORT, function(){
     console.log("Listening on port %s", PORT);
